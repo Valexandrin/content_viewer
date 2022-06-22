1f4e56cc5fc64d4e4d6cc385425ba56bceeddf88
Valexandrin <alex-vadim@mail.ru>
2022-06-22 12:59:20 +0300

    dataset params were added into AppConfig

--- frontend/config.py
+++ frontend/config.py
@@ -1,6 +1,7 @@
 import os
 
 from pydantic import BaseModel
+from frontend.data.data_processing import create_list
 
 
 class Server(BaseModel):
@@ -8,16 +9,24 @@ class Server(BaseModel):
     port: int
 
 
+class DataList(BaseModel):
+    images: list
+    shows: int
+    categories: set
+
 class AppConfig(BaseModel):
     server: Server
+    data: DataList
 
 
 
 def load_from_env() -> AppConfig:
     app_port = os.environ['APP_PORT']
     app_host = os.environ['APP_HOST']
+    images_list, total_shows, all_categories = create_list(os.environ['CSV_PATH'])
     return AppConfig(
         server=Server(port=app_port, host=app_host),
+        data=DataList(images=images_list, shows=total_shows, categories=all_categories),
     )
 
 
